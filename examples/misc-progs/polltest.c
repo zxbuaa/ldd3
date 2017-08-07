0ff53c75bc1941e861a4b24cd17a3f5d1089fe3b
Xiaobo Zhang <zxbuaa@gmail.com>
2017-08-07 14:15:58 +0800

    polltest replace poll with select
    Signed-off-by: Xiaobo Zhang <zxbuaa@gmail.com>

--- examples/misc-progs/polltest.c
+++ examples/misc-progs/polltest.c
@@ -20,28 +20,46 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <sys/poll.h>
+// #include <sys/poll.h>
+#include <sys/select.h>
 #include <fcntl.h>
 
 char buffer[4096];
 
 int main(int argc, char **argv)
 {
-    struct pollfd pfd;
+    // struct pollfd pfd;
+    fd_set rfds;
+    struct timeval tv;
     int n;
 
     fcntl(0, F_SETFL, fcntl(0,F_GETFL) | O_NONBLOCK); /* stdin */
-    pfd.fd = 0;  /* stdin */
-    pfd.events = POLLIN;
+    // pfd.fd = STDIN_FILENO;  /* stdin */
+    // pfd.events = POLLIN;
 
     while (1) {
-        n=read(0, buffer, 4096);
-        if (n >= 0)
-            write(1, buffer, n);
-	n = poll(&pfd, 1, -1);
-	if (n < 0)
-	    break;
+        // n = poll(&pfd, 1, -1);
+        do {
+            FD_ZERO(&rfds);
+            FD_SET(STDIN_FILENO, &rfds);
+            tv.tv_sec = 5;
+            tv.tv_usec = 0;
+            n = select(STDIN_FILENO + 1, &rfds, NULL, NULL, &tv);
+        } while (n == 0);
+        if (n < 0) {
+            perror("select");
+            break;
+        }
+        n = read(STDIN_FILENO, buffer, 4096);
+        if (n < 0) {
+            perror("stdin");
+            break;
+        }
+        n = write(STDOUT_FILENO, buffer, n);
+        if (n < 0) {
+            perror("stdout");
+            break;
+        }
     }
-    perror( n<0 ? "stdin" : "stdout");
     exit(1);
 }
